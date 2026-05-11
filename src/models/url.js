1f227d597ccaca811de6d1a401d3a79379ef3f1c
Aryan yadav <aryanyadav1217@gmail.com>
2026-05-12 00:37:50 +0530

    pushing a new files

---
+++ src/models/url.js
@@ -0,0 +1,10 @@
+import { Schema, model } from "mongoose";
+
+const urlSchema = new Schema({
+  originalUrl: { type: String, required: true },
+  shortCode:   { type: String, required: true, unique: true },
+  clicks:      { type: Number, default: 0 },
+  createdAt:   { type: Date, default: Date.now }
+});
+
+export default model("Url", urlSchema);
