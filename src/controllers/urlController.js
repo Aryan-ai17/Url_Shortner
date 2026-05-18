7193e38575838f7691401c2542d62926686f2bb9
Aryan yadav <aryanyadav1217@gmail.com>
2026-05-19 01:24:30 +0530

    Implement URL shortening and MongoDb storage

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -1,15 +1,35 @@
+import dotenv from "dotenv"
+dotenv.config()
+
 import validUrl from "valid-url"
 import { nanoid } from "nanoid"
-const createShortUrl=async (req,res)=>{
-    const {originalUrl}= req.body
+import Url from "../models/Url.js"
+
+const createShortUrl = async (req, res) => {
+    const { originalUrl } = req.body
+
     if (!validUrl.isWebUri(originalUrl)) {
-    return res.status(400).json({
-        message: "Invalid URL"
+        return res.status(400).json({
+            message: "Invalid URL"
+        })
+    }
+
+    const shortCode = nanoid(6)
+
+    const shortUrl =
+        `${process.env.BASE_URL}/${shortCode}`
+
+    const newUrl = new Url({
+        originalUrl,
+        shortCode,
+        shortUrl
+    })
+
+    await newUrl.save()
+
+    res.status(201).json({
+        shortUrl
     })
 }
-} 
 
-const shortCode=nanoid(6)
-console.log(shortCode)
-export {createShortUrl}
-
+export { createShortUrl }
