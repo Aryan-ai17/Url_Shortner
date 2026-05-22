aad7d13715bb3b1c31d5a3da304f1a8ad8e271aa
Aryan yadav <aryanyadav1217@gmail.com>
2026-05-23 00:26:31 +0530

    Add QR code generation and auth setup

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -4,6 +4,7 @@ dotenv.config()
 import validUrl from "valid-url"
 import { nanoid } from "nanoid"
 import Url from "../models/Url.js"
+import QRCode from "qrcode"
 
 const createShortUrl = async (req, res) => {
     try{
@@ -17,17 +18,23 @@ const createShortUrl = async (req, res) => {
     const existingUrl = await Url.findOne({
         originalUrl
     })
-    if (existingUrl){
-        return res.status(200).json({
-            shortUrl:existingUrl.shortUrl
-        })
-    }
+    if (existingUrl) {
+
+    const qrCode =
+        await QRCode.toDataURL(existingUrl.shortUrl)
+
+    return res.status(200).json({
+        shortUrl: existingUrl.shortUrl,
+        qrCode
+    })
+}
 
 
     const shortCode = nanoid(6)
 
     const shortUrl =
         `${process.env.BASE_URL}/${shortCode}`
+    const qrCode= await QRCode.toDataURL(shortUrl)   
 
     const newUrl = new Url({
         originalUrl,
@@ -38,7 +45,8 @@ const createShortUrl = async (req, res) => {
     await newUrl.save()
 
     res.status(201).json({
-        shortUrl
+        shortUrl,
+        qrCode
     })
 }
    catch(error){
