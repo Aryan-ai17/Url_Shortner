a0924b535a14ec7d789df2810669c1e57a47b193
Aryan yadav <aryanyadav1217@gmail.com>
2026-05-28 00:39:57 +0530

    Add JWT auth and protected routes

---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,34 @@
+import jwt from "jsonwebtoken"
+
+const authMiddleware = async (req, res, next) => {
+    try {
+        console.log(req.headers.authorization)
+
+
+        const token = req.headers.authorization?.split(" ")[1]
+        
+
+        if (!token) {
+            return res.status(401).json({
+                message: "Access Denied"
+            })
+        }
+
+        const decoded = jwt.verify(
+            token,
+            process.env.JWT_SECRET
+        )
+
+        req.user = decoded
+
+        next()
+
+    } catch (error) {
+
+        return res.status(401).json({
+            message: "Invalid Token"
+        })
+    }
+}
+
+export default authMiddleware
--- src/routers/urlRouters.js
+++ src/routers/urlRouters.js
@@ -1,9 +1,12 @@
 import express from "express"
+import { createShortUrl } from "../controllers/urlController.js"
+import { getUrlStats } from "../controllers/urlController.js"
+import authMiddleware from "../middleware/auth.middleware.js"
 const router =express.Router()
-router.post("/shorten",createShortUrl)
+router.post("/shorten",authMiddleware,createShortUrl)
 router.get("/stats/:shortCode",getUrlStats)
 
 
+
+
 export default router
-import { createShortUrl } from "../controllers/urlController.js"
-import { getUrlStats } from "../controllers/urlController.js"
